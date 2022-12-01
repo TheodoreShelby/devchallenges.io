1d2e47e84f3b422f24869c13e5f3f75195d85e33
Thang Nguyen Vu <nguyenvuthang942008@gmail.com>
2022-12-01 13:52:27 +0700

    complete creating UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import featureImage from "./assets/images/Scarecrow.png";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <section className="container">
+          <h3 className="text-upper">404 not found</h3>
+          <div className="content d-flex">
+            <div className="feature-image">
+              <img src={featureImage} alt="feature" />
+            </div>
+            <div className="message">
+              <h1>I have bad news for you</h1>
+              <p>
+                The page you are looking for might be removed or is temporarily
+                unavailable
+              </p>
+              <button className="text-upper">Back to homepage</button>
+            </div>
+          </div>
+        </section>
+        <footer>created by username - devChallenges.io</footer>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
